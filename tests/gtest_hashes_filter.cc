5ad7ba0d11f61c44e54622685a84aa8df8617d92
Axlgrep <wuxianjian-iri@360.com>
2018-01-30 20:50:53 +0800

    adjust code format

--- tests/gtest_hashes_filter.cc
+++ tests/gtest_hashes_filter.cc
@@ -63,8 +63,8 @@ TEST(HashesFilterTest, FilterTest) {
   HashesMetaValue tmf_meta_value1(std::string(str, sizeof(int32_t)));
   tmf_meta_value1.UpdateVersion();
   std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-  filter_result = hashes_meta_filter->Filter(0,
-          "FILTER_TEST_KEY", tmf_meta_value1.Encode(), &new_value, &value_changed);
+  filter_result = hashes_meta_filter->Filter(0, "FILTER_TEST_KEY",
+      tmf_meta_value1.Encode(), &new_value, &value_changed);
   ASSERT_EQ(filter_result, true);
 
   // No timeout is set, it's not an empty hash table.
@@ -72,8 +72,8 @@ TEST(HashesFilterTest, FilterTest) {
   HashesMetaValue tmf_meta_value2(std::string(str, sizeof(int32_t)));
   tmf_meta_value2.UpdateVersion();
   std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-  filter_result = hashes_meta_filter->Filter(0,
-          "FILTER_TEST_KEY", tmf_meta_value2.Encode(), &new_value, &value_changed);
+  filter_result = hashes_meta_filter->Filter(0, "FILTER_TEST_KEY",
+      tmf_meta_value2.Encode(), &new_value, &value_changed);
   ASSERT_EQ(filter_result, false);
 
   // timeout timestamp is set, but not timeout.
@@ -82,8 +82,8 @@ TEST(HashesFilterTest, FilterTest) {
   tmf_meta_value3.UpdateVersion();
   std::this_thread::sleep_for(std::chrono::milliseconds(1000));
   tmf_meta_value3.SetRelativeTimestamp(1);
-  filter_result = hashes_meta_filter->Filter(0,
-          "FILTER_TEST_KEY", tmf_meta_value3.Encode(), &new_value, &value_changed);
+  filter_result = hashes_meta_filter->Filter(0, "FILTER_TEST_KEY",
+      tmf_meta_value3.Encode(), &new_value, &value_changed);
   ASSERT_EQ(filter_result, false);
 
   // timeout timestamp is set, already timeout.
@@ -92,100 +92,115 @@ TEST(HashesFilterTest, FilterTest) {
   tmf_meta_value4.UpdateVersion();
   tmf_meta_value4.SetRelativeTimestamp(1);
   std::this_thread::sleep_for(std::chrono::milliseconds(2000));
-  filter_result = hashes_meta_filter->Filter(0,
-          "FILTER_TEST_KEY", tmf_meta_value4.Encode(), &new_value, &value_changed);
+  filter_result = hashes_meta_filter->Filter(0, "FILTER_TEST_KEY",
+      tmf_meta_value4.Encode(), &new_value, &value_changed);
   ASSERT_EQ(filter_result, true);
   delete hashes_meta_filter;
 
 
   /*************** TEST DATA FILTER ***************/
 
-  // No timeout is set, version not outmoded
-  HashesDataFilter* hashes_data_filter1 = new HashesDataFilter(meta_db, &handles);
+  // No timeout is set, version not outmoded.
+  HashesDataFilter* hashes_data_filter1
+    = new HashesDataFilter(meta_db, &handles);
   ASSERT_TRUE(hashes_data_filter1 != nullptr);
   EncodeFixed32(str, 1);
   HashesMetaValue tdf_meta_value1(std::string(str, sizeof(int32_t)));
   version = tdf_meta_value1.UpdateVersion();
-  s = meta_db->Put(rocksdb::WriteOptions(), handles[0], "FILTER_TEST_KEY", tdf_meta_value1.Encode());
+  s = meta_db->Put(rocksdb::WriteOptions(), handles[0],
+      "FILTER_TEST_KEY", tdf_meta_value1.Encode());
   ASSERT_TRUE(s.ok());
   HashesDataKey tdf_data_key1("FILTER_TEST_KEY", version, "FILTER_TEST_FIELD");
-  filter_result = hashes_data_filter1->Filter(0,
-          tdf_data_key1.Encode(), "FILTER_TEST_VALUE", &new_value, &value_changed);
+  filter_result = hashes_data_filter1->Filter(0, tdf_data_key1.Encode(),
+      "FILTER_TEST_VALUE", &new_value, &value_changed);
   ASSERT_EQ(filter_result, false);
-  s = meta_db->SingleDelete(rocksdb::WriteOptions(), handles[0], "FILTER_TEST_KEY");
+  s = meta_db->Delete(rocksdb::WriteOptions(),
+      handles[0], "FILTER_TEST_KEY");
   ASSERT_TRUE(s.ok());
   delete hashes_data_filter1;
 
-  // timeout timestamp is set, but not timeout
-  HashesDataFilter* hashes_data_filter2 = new HashesDataFilter(meta_db, &handles);
+  // timeout timestamp is set, but not timeout.
+  HashesDataFilter* hashes_data_filter2
+    = new HashesDataFilter(meta_db, &handles);
   ASSERT_TRUE(hashes_data_filter2 != nullptr);
   EncodeFixed32(str, 1);
   HashesMetaValue tdf_meta_value2(std::string(str, sizeof(int32_t)));
   version = tdf_meta_value2.UpdateVersion();
   tdf_meta_value2.SetRelativeTimestamp(1);
-  s = meta_db->Put(rocksdb::WriteOptions(), handles[0], "FILTER_TEST_KEY", tdf_meta_value2.Encode());
+  s = meta_db->Put(rocksdb::WriteOptions(), handles[0],
+      "FILTER_TEST_KEY", tdf_meta_value2.Encode());
   ASSERT_TRUE(s.ok());
   HashesDataKey tdf_data_key2("FILTER_TEST_KEY", version, "FILTER_TEST_FIELD");
-  filter_result = hashes_data_filter2->Filter(0,
-          tdf_data_key2.Encode(), "FILTER_TEST_VALUE", &new_value, &value_changed);
+  filter_result = hashes_data_filter2->Filter(0, tdf_data_key2.Encode(),
+      "FILTER_TEST_VALUE", &new_value, &value_changed);
   ASSERT_EQ(filter_result, false);
-  s = meta_db->SingleDelete(rocksdb::WriteOptions(), handles[0], "FILTER_TEST_KEY");
+  s = meta_db->Delete(rocksdb::WriteOptions(),
+      handles[0], "FILTER_TEST_KEY");
   ASSERT_TRUE(s.ok());
   delete hashes_data_filter2;
 
-  // timeout timestamp is set, already timeout
-  HashesDataFilter* hashes_data_filter3 = new HashesDataFilter(meta_db, &handles);
+  // timeout timestamp is set, already timeout.
+  HashesDataFilter* hashes_data_filter3
+    = new HashesDataFilter(meta_db, &handles);
   ASSERT_TRUE(hashes_data_filter3 != nullptr);
   EncodeFixed32(str, 1);
   HashesMetaValue tdf_meta_value3(std::string(str, sizeof(int32_t)));
   version = tdf_meta_value3.UpdateVersion();
   tdf_meta_value3.SetRelativeTimestamp(1);
-  s = meta_db->Put(rocksdb::WriteOptions(), handles[0], "FILTER_TEST_KEY", tdf_meta_value3.Encode());
+  s = meta_db->Put(rocksdb::WriteOptions(), handles[0],
+      "FILTER_TEST_KEY", tdf_meta_value3.Encode());
   ASSERT_TRUE(s.ok());
   std::this_thread::sleep_for(std::chrono::milliseconds(2000));
   HashesDataKey tdf_data_key3("FILTER_TEST_KEY", version, "FILTER_TEST_FIELD");
-  filter_result = hashes_data_filter3->Filter(0,
-          tdf_data_key3.Encode(), "FILTER_TEST_VALUE", &new_value, &value_changed);
+  filter_result = hashes_data_filter3->Filter(0, tdf_data_key3.Encode(),
+      "FILTER_TEST_VALUE", &new_value, &value_changed);
   ASSERT_EQ(filter_result, true);
-  s = meta_db->SingleDelete(rocksdb::WriteOptions(), handles[0], "FILTER_TEST_KEY");
+  s = meta_db->Delete(rocksdb::WriteOptions(),
+      handles[0], "FILTER_TEST_KEY");
   ASSERT_TRUE(s.ok());
   delete hashes_data_filter3;
 
-  // No timeout is set, version outmoded
-  HashesDataFilter* hashes_data_filter4 = new HashesDataFilter(meta_db, &handles);
+  // No timeout is set, version outmoded.
+  HashesDataFilter* hashes_data_filter4
+    = new HashesDataFilter(meta_db, &handles);
   ASSERT_TRUE(hashes_data_filter4 != nullptr);
   EncodeFixed32(str, 1);
   HashesMetaValue tdf_meta_value4(std::string(str, sizeof(int32_t)));
   version = tdf_meta_value4.UpdateVersion();
-  s = meta_db->Put(rocksdb::WriteOptions(), handles[0], "FILTER_TEST_KEY", tdf_meta_value4.Encode());
+  s = meta_db->Put(rocksdb::WriteOptions(), handles[0],
+      "FILTER_TEST_KEY", tdf_meta_value4.Encode());
   ASSERT_TRUE(s.ok());
   HashesDataKey tdf_data_key4("FILTER_TEST_KEY", version, "FILTER_TEST_FIELD");
   version = tdf_meta_value4.UpdateVersion();
-  s = meta_db->Put(rocksdb::WriteOptions(), handles[0], "FILTER_TEST_KEY", tdf_meta_value4.Encode());
+  s = meta_db->Put(rocksdb::WriteOptions(), handles[0],
+      "FILTER_TEST_KEY", tdf_meta_value4.Encode());
   ASSERT_TRUE(s.ok());
-  filter_result = hashes_data_filter4->Filter(0,
-          tdf_data_key4.Encode(), "FILTER_TEST_VALUE", &new_value, &value_changed);
+  filter_result = hashes_data_filter4->Filter(0, tdf_data_key4.Encode(),
+      "FILTER_TEST_VALUE", &new_value, &value_changed);
   ASSERT_EQ(filter_result, true);
-  s = meta_db->SingleDelete(rocksdb::WriteOptions(), handles[0], "FILTER_TEST_KEY");
+  s = meta_db->Delete(rocksdb::WriteOptions(),
+      handles[0], "FILTER_TEST_KEY");
   ASSERT_TRUE(s.ok());
   delete hashes_data_filter4;
 
-  // Hash table meta data has been clear
-  HashesDataFilter* hashes_data_filter5 = new HashesDataFilter(meta_db, &handles);
+  // Hash table meta data has been clear.
+  HashesDataFilter* hashes_data_filter5
+    = new HashesDataFilter(meta_db, &handles);
   ASSERT_TRUE(hashes_data_filter5 != nullptr);
   EncodeFixed32(str, 1);
   HashesMetaValue tdf_meta_value5(std::string(str, sizeof(int32_t)));
   version = tdf_meta_value5.UpdateVersion();
-  s = meta_db->Put(rocksdb::WriteOptions(), handles[0], "FILTER_TEST_KEY", tdf_meta_value5.Encode());
+  s = meta_db->Put(rocksdb::WriteOptions(), handles[0],
+      "FILTER_TEST_KEY", tdf_meta_value5.Encode());
   ASSERT_TRUE(s.ok());
   HashesDataKey tdf_data_key5("FILTER_TEST_KEY", version, "FILTER_TEST_FIELD");
-  s = meta_db->SingleDelete(rocksdb::WriteOptions(), handles[0], "FILTER_TEST_KEY");
+  s = meta_db->Delete(rocksdb::WriteOptions(),
+      handles[0], "FILTER_TEST_KEY");
   ASSERT_TRUE(s.ok());
-  filter_result = hashes_data_filter5->Filter(0,
-          tdf_data_key5.Encode(), "FILTER_TEST_VALUE", &new_value, &value_changed);
+  filter_result = hashes_data_filter5->Filter(0, tdf_data_key5.Encode(),
+      "FILTER_TEST_VALUE", &new_value, &value_changed);
   ASSERT_EQ(filter_result, true);
   delete hashes_data_filter5;
-
 }
 
 int main(int argc, char** argv) {
